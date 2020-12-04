32645eaca900ac4621e848f588f756c1d57fc5e7
Spencer Hardy <carsandtrains@gmail.com>
2020-12-04 00:12:50 -0800

    added firebase functionality

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fluttershare/widgets/header.dart';
-import 'package:fluttershare/widgets/progress.dart';
+
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -9,10 +11,26 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
   @override
+  void initState() {
+    getUsers();
+    super.initState();
+  }
+
+  getUsers() {
+    usersRef.getDocuments().then((QuerySnapshot snapshot) {
+      snapshot.documents.forEach((DocumentSnapshot doc) {
+        print(doc.data);
+        print(doc.documentID);
+        print(doc.exists);
+      });
+    });
+  }
+
+  @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: linearProgress(), 
-      );
+      body: Text('Timeline'),
+    );
   }
 }
